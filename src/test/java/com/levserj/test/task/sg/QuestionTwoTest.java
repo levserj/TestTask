0f6cce97ab79eb7ced69f240a3e65ee74dd61153
levserj <levserj@yahoo.com>
2016-10-12 11:20:33 +0300

    - QuestionTwo test added

---
+++ src/test/java/com/levserj/test/task/sg/QuestionTwoTest.java
@@ -0,0 +1,25 @@
+package com.levserj.test.task.sg;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * Created by Levchynskyi Serhii on 11.10.16.
+ */
+public class QuestionTwoTest {
+
+    private QuestionTwo questionTwo;
+
+    @Before
+    public void setUp(){
+        questionTwo = new QuestionTwo();
+    }
+
+    @Test
+    public void checkMethodFindPalindromeReturnsCorrectInt(){
+        assertThat(questionTwo.nextPalindromeFinder(15), is(51));
+    }
+}
